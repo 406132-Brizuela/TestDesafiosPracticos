5532a3f93c4dfb3a0d564a1f93fc1d9c3981daaa
Tomas Brizuela <406132@tecnicatura.frc.utn.edu.ar>
2026-09-17 02:31:06 -0300

    Perfil standalone

--- backend/src/main/java/com/tp/desafiospracticos/config/SecurityConfig.java
+++ backend/src/main/java/com/tp/desafiospracticos/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -22,6 +23,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
  */
 @Configuration
 @EnableMethodSecurity
+@Profile("!standalone")
 public class SecurityConfig {
 
     @Bean
